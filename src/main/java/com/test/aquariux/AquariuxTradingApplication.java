0454059e3b259b407ac97bec12344a3ba106014e
hoc.ha <hoc.ha@saigontechnology.com>
2025-01-04 18:43:54 +0700

    Initial commit

---
+++ src/main/java/com/test/aquariux/AquariuxTradingApplication.java
@@ -0,0 +1,13 @@
+package com.test.aquariux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariuxTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariuxTradingApplication.class, args);
+	}
+
+}
